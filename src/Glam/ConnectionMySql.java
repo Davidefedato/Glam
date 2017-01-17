ff1aa1bcfd4bdc9721cd584a081aca835807ba05
bassomatteo <bassomatteo@Lab06_13.itseinaudi.local>
2017-01-17 11:43:54 +0100

    filtro data

--- src/Glam/ConnectionMySql.java
+++ src/Glam/ConnectionMySql.java
@@ -15,6 +15,7 @@ public class ConnectionMySql {
 	static int ru;
 	static String sql;
 	public ArrayList<Utente> u = new ArrayList<Utente>();
+	public ArrayList<Utente> uf = new ArrayList<Utente>();
 
 	public void Connection() throws SQLException {
 		u.clear();
@@ -44,7 +45,41 @@ public class ConnectionMySql {
 		}
 
 	}
-	
+
+	public void filtroOra(String sql) throws IOException {
+		Connection cn;
+		Statement st;
+		// ________________________________connessione
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.err.println(e.getMessage());
+		} // fine try-catch
+
+		try {
+			// Creo la connessione al database
+			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/glam?user=root&password=");
+
+			System.out.println(sql);
+			// ________________________________query
+
+			st = cn.createStatement(); // creo sempre uno statement sulla
+										// connessione
+			try {
+				st = cn.createStatement();
+				rs = st.executeQuery(sql);
+				while (rs.next() == true) {
+					uf.add(new Utente(rs.getString("Nome"), rs.getString("Cognome"), rs.getString("Data")));
+				}
+			} catch (SQLException e) {
+				System.out.println("errore:" + e.getMessage());
+			}
+			cn.close(); // chiusura connessione
+		} catch (SQLException e) {
+			System.out.println("errore:" + e.getMessage());
+		} // fine try-catch
+	}
 
 	public void nuovoUtente(String sql) throws IOException {
 		Connection cn;
--- src/Glam/GraficaServer.java
+++ src/Glam/GraficaServer.java
@@ -3,13 +3,36 @@ package Glam;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.custom.TableCursor;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class GraficaServer {
 
 	protected Shell shell;
-	private Table table;
+	List list;
+	Server s = new Server();
+	private Text txtOra;
+	ConnectionMySql con = new ConnectionMySql();
+	String sql;
+	ArrayList<Utente> elenco = new ArrayList<Utente>();
+	ArrayList<Utente> uf = new ArrayList<Utente>();
 
 	/**
 	 * Launch the application.
@@ -47,22 +70,62 @@ public class GraficaServer {
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
 		
-		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setBounds(29, 10, 306, 242);
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
+		try {
+			con.Connection();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		TableColumn tblclmnNome = new TableColumn(table, SWT.NONE);
-		tblclmnNome.setWidth(100);
-		tblclmnNome.setText("Nome");
+		list = new List(shell, SWT.BORDER);
+		list.setBounds(10, 10, 201, 242);
 		
-		TableColumn tblclmnCognome = new TableColumn(table, SWT.NONE);
-		tblclmnCognome.setWidth(100);
-		tblclmnCognome.setText("Cognome");
+		elenco = con.u;
 		
-		TableColumn tblclmnData = new TableColumn(table, SWT.NONE);
-		tblclmnData.setWidth(100);
-		tblclmnData.setText("Data");
-
+		for(int i=0;i<elenco.size();i++){
+			list.add(elenco.get(i).nome+ " " +elenco.get(i).cognome );
+		}
+		
+		Label lblData = new Label(shell, SWT.NONE);
+		lblData.setAlignment(SWT.RIGHT);
+		lblData.setBounds(234, 14, 55, 15);
+		lblData.setText("Data:");
+		
+		DateTime dateTime = new DateTime(shell, SWT.BORDER);
+		dateTime.setBounds(295, 10, 80, 24);
+		
+		Label lblOra = new Label(shell, SWT.NONE);
+		lblOra.setAlignment(SWT.RIGHT);
+		lblOra.setBounds(234, 43, 55, 15);
+		lblOra.setText("Ora:");
+		
+		txtOra = new Text(shell, SWT.BORDER);
+		txtOra.setBounds(295, 40, 80, 21);
+		
+		Button btnGet = new Button(shell, SWT.NONE);
+		btnGet.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				list.removeAll();
+				java.text.DateFormat dfin = new SimpleDateFormat("yyyy-MM-dd");
+				String data;
+				data = dateTime.getYear() + "-" + dateTime.getMonth() + "-" + dateTime.getDay()+" "+txtOra.getText();
+				System.out.println(data);
+				sql =" SELECT * FROM `utente` WHERE `Data` < '"+data+"';";
+				try {
+					con.filtroOra(sql);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				uf = con.uf;
+				for(int i=0;i<uf.size();i++){
+					list.add(uf.get(i).nome+ " " +uf.get(i).cognome);
+				}
+			}
+		});
+		btnGet.setBounds(283, 89, 75, 25);
+		btnGet.setText("Get");		
+		
 	}
 }
