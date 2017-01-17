36c4e9ae63dc8c94f5795b9ad46f7b1eaeb6f08c
bassomatteo <bassomatteo@Lab06_13.itseinaudi.local>
2017-01-17 11:19:40 +0100

    table

--- src/Glam/GraficaServer.java
+++ src/Glam/GraficaServer.java
@@ -2,10 +2,14 @@ package Glam;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.TableColumn;
 
 public class GraficaServer {
 
 	protected Shell shell;
+	private Table table;
 
 	/**
 	 * Launch the application.
@@ -42,7 +46,23 @@ public class GraficaServer {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
+		
+		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(29, 10, 306, 242);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnNome = new TableColumn(table, SWT.NONE);
+		tblclmnNome.setWidth(100);
+		tblclmnNome.setText("Nome");
+		
+		TableColumn tblclmnCognome = new TableColumn(table, SWT.NONE);
+		tblclmnCognome.setWidth(100);
+		tblclmnCognome.setText("Cognome");
+		
+		TableColumn tblclmnData = new TableColumn(table, SWT.NONE);
+		tblclmnData.setWidth(100);
+		tblclmnData.setText("Data");
 
 	}
-
 }
