336a3e988ab724c5060ba500ca3a0617cb24ac20
bassomatteo <bassomatteo@Lab06_13.itseinaudi.local>
2017-01-19 08:16:26 +0100

    controllo iscrizione

--- src/Glam/Server.java
+++ src/Glam/Server.java
@@ -6,17 +6,28 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Server {
+	
+	public static ArrayList<Utente> elenco = new ArrayList<Utente>();
 
 	public static void main(String[] args) {
 		String nomeRicevuto;
 		String cognomeRicevuto;
 		String doRicevuto;
 		String sql;
-		ArrayList<Utente> elenco = new ArrayList<Utente>();
 		ConnectionMySql con = new ConnectionMySql();
+		
+		try {
+			con.Connection();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		elenco = con.u;
 
 		// riceva una connessione
 
@@ -35,6 +46,7 @@ public class Server {
 				doRicevuto = in.readLine();
 
 				boolean esiste = false;
+				System.out.println(elenco.size());
 
 				for (int i = 0; i < elenco.size(); i++) {
 					if (elenco.get(i).nome.equals(nomeRicevuto)) {
@@ -49,7 +61,7 @@ public class Server {
 							+ "', '" + cognomeRicevuto + "', '" + doRicevuto + "');";
 					con.nuovoUtente(sql);
 					System.out.println(
-							"Nome : " + nomeRicevuto + "Cognome : " + cognomeRicevuto + "Data : " + doRicevuto);
+							"Nome : " + nomeRicevuto + " Cognome : " + cognomeRicevuto + " Data : " + doRicevuto);
 
 				} else {
 					out.println("Già iscritto!");
